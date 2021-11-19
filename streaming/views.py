e480c345cbb8d88ccb34f107fd8f63e6b919e1dc
dungbik <q1w2e30630@gmail.com>
2021-11-19 20:51:45 +0900

    Feat: 템플릿을 통해 스트리밍

--- streaming/views.py
+++ streaming/views.py
@@ -1,8 +1,13 @@
 from django.http import StreamingHttpResponse
+from django.shortcuts import render
 
 from streaming.video import gen, VideoCamera
 
 
 # Create your views here.
 def index(request):
+    return render(request, 'index.html')
+
+
+def video(request):
     return StreamingHttpResponse(gen(VideoCamera()), content_type='multipart/x-mixed-replace; boundary=frame')
